321a6f04f07a9cd46710c2fa314c1e6f4e24c990
Sebastian Sobiech <eslaron90@gmail.com>
2022-02-01 18:28:20 +0100

    Initial Spring boostrap: - Some dependecies may be removed at a later time - For now I grabbed what seemed useful.

---
+++ src/main/kotlin/com/tcisoftware/sobiech/warehouse/WarehouseApplication.kt
@@ -0,0 +1,11 @@
+package com.tcisoftware.sobiech.warehouse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WarehouseApplication
+
+fun main(args: Array<String>) {
+	runApplication<WarehouseApplication>(*args)
+}
